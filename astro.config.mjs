c2901f26abb7b58be6044fcc9bd1cbe6f0f3fa00
Marcus Weber <marcus.wallau@gmail.com>
2024-12-04 20:59:17 +0100

    chore: change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://marweumr.github.io",
-  base: "wiki.me",
+  base: "/",
   integrations: [
     starlight({
       title: "My Docs",
