f19227df36dfdb0b0bdf13275987c82716205811
Marcus Weber <marcus.wallau@gmail.com>
2024-12-04 20:44:04 +0100

    feat: deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://marweumr.github.io",
+  base: "wiki.me",
   integrations: [
     starlight({
       title: "My Docs",
