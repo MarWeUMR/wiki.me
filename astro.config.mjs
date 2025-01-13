dcd1897a0f9bd8131e3928cc9beb50f6e2375c8b
Marcus Weber <marcus.wallau@gmail.com>
2025-01-13 17:17:28 +0100

    chore: try different base setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import starlight from "@astrojs/starlight";
 const isProd = process.env.NODE_ENV === "production";
 // https://astro.build/config
 export default defineConfig({
-  site: isProd ? "https://marweumr.github.io/wiki.me" : "http://localhost:4321",
-  base: isProd ? "/wiki.me" : "/",
+  site: isProd
+    ? "https://marweumr.github.io/wiki.me/"
+    : "http://localhost:4321",
+  base: isProd ? "/wiki.me/" : "/",
   // site: "https://marweumr.github.io",
   // base: "/wiki.me",
   integrations: [
